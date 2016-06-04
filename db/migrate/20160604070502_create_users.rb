4598712b6a552192c842485d35ee182c0aa6ad54
Tsehau Chao <jodeci@5xruby.tw>
2016-06-04 15:36:37 +0800

    add basic devise on User

---
+++ db/migrate/20160604070502_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :name, null: false, default: ''
+      t.string :login_name, null: false, unique: true
+      t.string :role, default: 'pending'
+      t.date :join_date
+      t.date :leave_date
+
+      t.timestamps
+    end
+  end
+end
