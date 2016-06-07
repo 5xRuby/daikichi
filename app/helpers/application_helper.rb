8016c815921ad1728533b52ca2ac5a9df6429625
Tsehau Chao <jodeci@5xruby.tw>
2016-06-07 13:26:07 +0800

    template fixing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def dropdown_title(label = '')
+    capture_haml do
+      haml_tag 'a.dropdown-toggle', {'data-toggle': 'dropdown', 
+                                     'role': 'button', 
+                                     'aria-haspopup': true,
+                                     'aria-expanded': 'false'} do
+        haml_concat label
+        haml_tag 'span.caret'
+      end
+    end
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
