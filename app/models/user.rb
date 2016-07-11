b115ddb583ae80ff0e672316b309311d871dd8c9
Tsehau Chao <jodeci@5xruby.tw>
2016-07-11 18:46:36 +0800

    user edit & i18n roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  ROLES = %i[manager employee probation contractor vendor intern resigned pending]
+
 end
