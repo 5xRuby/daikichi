8f1fa5f0aac2cb0dfe0cfff7b02d6e35083ce250
Tsehau Chao <jodeci@5xruby.tw>
2016-07-12 14:58:53 +0800

    byebug remote comment

--- config/initializers/byebug.rb
+++ config/initializers/byebug.rb
@@ -1,3 +1,4 @@
+# bundle exec byebug -R localhost:{port}
 if Rails.env.development? and ENV['BYEBUGPORT']
   require 'byebug/core'
   Byebug.start_server 'localhost', ENV['BYEBUGPORT'].to_i #set in .powenv
