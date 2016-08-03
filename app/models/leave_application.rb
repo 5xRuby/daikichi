8738ca63a27d42d111bdccd8240215918c986a2b
Eric Lee <imdfmy@gmail.com>
2016-08-03 18:16:11 +0800

    when user apply for leave, system will deduct the usable_hours from leave_time

--- app/models/leave_application.rb
+++ app/models/leave_application.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 class LeaveApplication < ApplicationRecord
-  before_save :calculate_hours
   belongs_to :user
   belongs_to :manager, class_name: "User", foreign_key: "manager_id"
   validates_presence_of :leave_type, :description
@@ -35,10 +34,21 @@ class LeaveApplication < ApplicationRecord
 
   end
 
+  def calculate_hours_and_deduct_it_from_leave_time
+    leave_time = self.user.leave_times.where("leave_type = ?", self.leave_type).first
+    self.hours = calculate_hours
+    byebug
+    unless self.hours_was == self.hours
+      leave_time.usable_hours += self.hours_was
+      leave_time.usable_hours -= self.hours
+      leave_time.save!
+    end
+    save!
+  end
+
   private
 
   def calculate_hours
-    self.hours = ( end_time - start_time ) / 3600.0
-    puts end_time
+    ( end_time - start_time ) / 3600.0
   end
 end
