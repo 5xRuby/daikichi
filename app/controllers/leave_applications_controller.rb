227033aa6069efd804f39f8e6825d99d5e5e7952
Eric Lee <imdfmy@gmail.com>
2016-08-05 19:30:31 +0800

    set calculate_hours_and_deduct_it_from_leave_time to before_save method

--- app/controllers/leave_applications_controller.rb
+++ app/controllers/leave_applications_controller.rb
@@ -1,24 +1,5 @@
 class LeaveApplicationsController < BaseController
 
-  def create
-    current_object = collection_scope.new(resource_params)
-    return render action: :new unless current_object.save
-    current_object.calculate_hours_and_deduct_it_from_leave_time
-    action_success
-  end
-
-  def update
-    if current_object.update(resource_params)
-      current_object.calculate_hours_and_deduct_it_from_leave_time
-      action_success
-    else
-      respond_to do |f|
-        f.html { return render action: :edit }
-        f.json
-      end
-    end
-  end
-
   def collection_scope
     current_user.leave_applications
   end
--- app/models/leave_application.rb
+++ app/models/leave_application.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 class LeaveApplication < ApplicationRecord
+  before_save :calculate_hours_and_deduct_it_from_leave_time
   belongs_to :user
   belongs_to :manager, class_name: "User", foreign_key: "manager_id"
   validates_presence_of :leave_type, :description
@@ -34,19 +35,17 @@ class LeaveApplication < ApplicationRecord
 
   end
 
+  private
+
   def calculate_hours_and_deduct_it_from_leave_time
     leave_time = self.user.leave_times.where("leave_type = ?", self.leave_type).first
     self.hours = calculate_hours
-    byebug
     unless self.hours_was == self.hours
       leave_time.usable_hours += self.hours_was
       leave_time.usable_hours -= self.hours
       leave_time.save!
     end
-    save!
   end
-
-  private
 
   def calculate_hours
     ( end_time - start_time ) / 3600.0
