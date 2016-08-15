ada19776706e5934484e1c54b1199ecbf8c945a0
Eric Lee <imdfmy@gmail.com>
2016-08-15 15:22:31 +0800

    datetimepicker end_time will auto show

--- app/assets/javascripts/leave_applications.js
+++ app/assets/javascripts/leave_applications.js
@@ -3,15 +3,18 @@ $( function(){
     format: 'YYYY-MM-DD HH:mm',
     enabledHours: [9, 10, 11, 12, 13, 14, 15, 16, 17, 18],
     stepping: 30,
+    useCurrent: false,
   });
   $('#datetimepicker_end').datetimepicker( {
     format: 'YYYY-MM-DD HH:mm',
     enabledHours: [9, 10, 11, 12, 13, 14, 15, 16, 17, 18],
     stepping: 30,
-    defaultDate: moment(new Date).set({hour: 10, minute: 0})
+    useCurrent: false,
   });
   $('#datetimepicker_start').on( 'dp.change', function(e){
     $('#datetimepicker_end').data('DateTimePicker').minDate( e.date.add(1, 'hours'));
+    var m = moment(new Date(e.date))
+    $('#datetimepicker_end').data('DateTimePicker').date(m);
   });
   $('#datetimepicker_end').on( 'dp.change', function(e){
     $('#datetimepicker_start').data('DateTimePicker').maxDate( e.date.subtract(1, 'hours'));
--- app/helpers/leave_applications_helper.rb
+++ app/helpers/leave_applications_helper.rb
@@ -2,4 +2,31 @@ module LeaveApplicationsHelper
   def convert_time_format(time_object, key = :default)
     time_object.to_s(key)
   end
+
+  def set_default_time(time_object, key)
+    return time_object if action_name == "edit"
+
+    time_object = Time.zone.now
+    if over? time_object, hour: 18
+      time_object = time_object.change day: next_business_(:day, time_object), hour: 9, min: 30
+    else
+      if over? time_object, min:30
+        time_object = time_object.change hour: next_business_(:hour, time_object), min: 0
+      else
+        time_object = time_object.change min: 30
+      end
+    end
+    byebug
+    (key==:end) ? (time_object = time_object.change hour: next_business_(:hour, time_object), min: 30) : time_object
+  end
+
+  def over?(time_object , options={})
+    result = true
+    options.each {|key, val| result = result && time_object.send(key).send(:>, val)}
+    result
+  end
+
+  def next_business_(key, time_object)
+    1.send("business_#{key.to_s}").after(time_object).send(key)
+  end
 end
