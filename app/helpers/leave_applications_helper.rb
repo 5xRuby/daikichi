c535aacf23fe606f48d008a7a1c4327b37b67172
Eric Lee <imdfmy@gmail.com>
2016-08-15 17:23:49 +0800

    revised leave_application helper functions' name

--- app/helpers/leave_applications_helper.rb
+++ app/helpers/leave_applications_helper.rb
@@ -3,20 +3,16 @@ module LeaveApplicationsHelper
     time_object.to_s(key)
   end
 
-  def new_input_hash(key, time_object = Time.zone.now)
-    if key==:start
-      time_object = time_object.change hour: 9, min: 30
+  def adjusted_time(key)
+    time_object = Time.zone.now.change(hour: 9, min: 30)
+    if key == :start
+      time_object.change(hour: 9, min: 30)
     else
-      time_object = time_object.change hour: 10, min: 30
+      time_object.change(hour: 10, min: 30)
     end
-    hash(key, time_object)
   end
 
-  def edit_input_hash(key, time_object)
-    hash(key, time_object)
-  end
-
-  def hash(key, time_object)
+  def date_time_picker_hash(key, time_object)
     {as: :date_time_picker, time: key.to_s, input_html: {value: time_object}, require: true}
   end
 end
