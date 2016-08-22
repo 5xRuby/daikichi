98d69f628bbdaef03f318c393bcbf2d31c62c952
Eric Lee <imdfmy@gmail.com>
2016-08-22 15:36:43 +0800

    added employee_leave_times controller index

--- app/controllers/backend/employee_leave_times_controller.rb
+++ app/controllers/backend/employee_leave_times_controller.rb
@@ -1,2 +1,14 @@
 class Backend::EmployeeLeaveTimesController < ApplicationController
+  def index
+  end
+
+  private
+
+  def collection_scope
+    if params[:id]
+      User
+    else
+      User.order(id: :desc)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
     namespace :backend do
       resources :users
+      resources :employee_leave_times, only: [:index]
       resources :leave_applications, only: [:index, :update] do
         member do
           get 'verify'
