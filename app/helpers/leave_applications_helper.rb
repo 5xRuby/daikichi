97fb0a4dc131e19ad09a245d0689b9ce73b233fd
Eric Lee <imdfmy@gmail.com>
2016-08-25 16:55:10 +0800

    Revised tr_by_object helper function, which is becoming general. It can tranform the value to specific format. corrected the meta.zh-TW file and all the corresponding views

--- app/helpers/leave_applications_helper.rb
+++ app/helpers/leave_applications_helper.rb
@@ -1,7 +1,4 @@
 module LeaveApplicationsHelper
-  def convert_time_format(time_object, key = :default)
-    time_object.to_s(key)
-  end
 
   def time_object_by_hour_min(hour, min)
     Time.zone.now.change(hour: hour, min: min)
@@ -9,9 +6,5 @@ module LeaveApplicationsHelper
 
   def date_time_picker_hash(key, time_object)
     {as: :date_time_picker, time: key.to_s, input_html: {value: time_object}, require: true}
-  end
-
-  def trans(value, scope)
-    t("misc.#{scope}.#{value}")
   end
 end
