9c1da18bd1f33bf15dcf8ace0d92643d7d830893
Eric Lee <imdfmy@gmail.com>
2016-08-29 12:33:09 +0800

    revised employee_leave_times controller, preventing it from automatically loading and authorizing the corresponding model

--- app/controllers/backend/employee_leave_times_controller.rb
+++ app/controllers/backend/employee_leave_times_controller.rb
@@ -1,8 +1,16 @@
-class Backend::EmployeeLeaveTimesController < ApplicationController
+class Backend::EmployeeLeaveTimesController < Backend::BaseController
+  before_action :custom_authorize
+  skip_load_and_authorize_resource
+
   def index
   end
 
   private
+
+  def custom_authorize
+    authorize! action_name, @current_collection
+    authorize! action_name, @current_object
+  end
 
   def collection_scope
     if params[:id]
