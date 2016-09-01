3b1ce6c4fbdee9fdcdcdd538d4c65e125e8b5ee9
Eric Lee <imdfmy@gmail.com>
2016-09-01 10:26:26 +0800

    leave_applications#new datetimepicker must cooperate with turbolink

--- app/assets/javascripts/leave_applications.js
+++ app/assets/javascripts/leave_applications.js
@@ -1,4 +1,4 @@
-$( function(){
+document.addEventListener("turbolinks:load", function() {
   $('#datetimepicker_start').datetimepicker( {
     format: 'YYYY-MM-DD HH:mm',
     enabledHours: [9, 10, 11, 12, 13, 14, 15, 16, 17, 18],
