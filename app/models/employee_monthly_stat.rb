9b46535b93d2598fa7fd4b5b0dfcd72dda68a8aa
Eric Lee <imdfmy@gmail.com>
2016-09-01 14:51:44 +0800

    revised employee_monthly_stat bug. Whenever there is a leave crossing a month, the system will automatically calculate the right total hours

--- app/models/employee_monthly_stat.rb
+++ app/models/employee_monthly_stat.rb
@@ -2,14 +2,23 @@ class EmployeeMonthlyStat
   def self.total_leave_times_hours(year, mon)
     result = CustomHash.new
 
-    range_begin_at = Time.zone.now.change(year: year, month: mon, day: 1, hour: 0)
-    range_end_at = Time.zone.now.change(year: year, month: mon).end_of_month
-    range = range_begin_at .. range_end_at
+    range_start = Time.zone.now.change(year: year, month: mon, day: 1, hour: 0)
+    range_end = Time.zone.now.change(year: year, month: mon).end_of_month
+    range = range_start .. range_end
 
-    leaves = LeaveApplication.where(sign_date: range, status: "approved")
+    leaves_with_start_time_in_range = LeaveApplication.where(start_time: range, status: "approved")
+    leaves_with_end_time_in_range = LeaveApplication.where(end_time: range, status: "approved")
+    leaves = leaves_with_start_time_in_range.or(leaves_with_end_time_in_range)
+
     leaves.each do |leave|
-      result[leave.user_id][leave.leave_type] += leave.hours
+      if range.cover?(leave.start_time) and range.cover?(leave.end_time)
+        result[leave.user_id][leave.leave_type] += leave.hours
+      elsif range.cover? leave.start_time
+        result[leave.user_id][leave.leave_type] += leave.start_time.working_time_until(range_end) / 3600
+      else
+        result[leave.user_id][leave.leave_type] += range_start.working_time_until(leave.end_time) / 3600
+      end
     end
-    return result
+    result
   end
 end
