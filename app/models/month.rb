9a70db0f751a7ee3f3171b37d9ca131b16c791fd
Eric Lee <imdfmy@gmail.com>
2016-09-01 10:28:18 +0800

    revised navigation bar employee_leave_times link and corresponding routes setting.

--- app/models/month.rb
+++ app/models/month.rb
@@ -1,7 +1,7 @@
 class Month
-  def self.leave_application_hours_stat(year=Time.zone.now.year, mon=Time.zone.now.mon)
+  def self.leave_application_hours_stat(year, mon)
     stat = Stat.new
-    year, mon = year.to_i, mon.to_i
+
     range_begin_at = Time.zone.now.change(year: year, month: mon, day: 1, hour: 0)
     range_end_at = Time.zone.now.change(year: year, month: mon).end_of_month
     range = range_begin_at .. range_end_at
--- config/routes.rb
+++ config/routes.rb
@@ -6,16 +6,12 @@ Rails.application.routes.draw do
 
     namespace :backend do
       resources :users
-      resources :employee_leave_times, only: [:index] do
-        collection do
-          get ':year/:month', to: 'employee_leave_times#index'
-        end
-      end
       resources :leave_applications, only: [:index, :update] do
         member do
           get 'verify'
         end
       end
+      get 'employee_leave_times/:year/:month', to: 'employee_leave_times#index', as: "employee_leave_times"
     end
     resources :leave_applications, except: [:destroy] do
       member do
