261b5dfd7d219b1bcc295bcd73cc2cf4e918889a
Tsehau Chao <jodeci@5xruby.tw>
2016-09-27 17:14:01 +0900

    mon->month

--- app/assets/javascripts/backend/employee_leave_times.js
+++ app/assets/javascripts/backend/employee_leave_times.js
@@ -1,25 +1,13 @@
 document.addEventListener("turbolinks:load", function() {
 
   var $year = $('select[id=year]')
-  var $mon = $('select[id=mon]')
+  var $month = $('select[id=month]')
 
   $year.on('change', function() {
-    Turbolinks.visit("/backend/employee_leave_times/" + $year.val() + "/" + $mon.val());
+    Turbolinks.visit("/backend/employee_leave_times/" + $year.val() + "/" + $month.val());
   });
 
-  $mon.on('change', function() {
-    Turbolinks.visit("/backend/employee_leave_times/" + $year.val() + "/" + $mon.val());
-
-    // 做法一, 連至 /backend/employee_leave_times/year/mon, 整頁換掉
-    //window.location.href = "/backend/employee_leave_times/" + $year.val() + "/" + $mon.val();
-
-
-    // 做法二, ajax 打一樣的 action, 但送回 view 時，是一個 json 檔
-    //$.get(
-      //"/backend/employee_leave_times/" + x + "/" + mon.val() + ".json",
-      //function(err, data) {
-        //console.log(data, err)
-      //}
-    //)
+  $month.on('change', function() {
+    Turbolinks.visit("/backend/employee_leave_times/" + $year.val() + "/" + $month.val());
   });
 });
