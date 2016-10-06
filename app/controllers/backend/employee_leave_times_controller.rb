fcc4b637b901cd860a50468797d199bedd23ce4f
Eric Lee <imdfmy@gmail.com>
2016-10-06 15:19:28 +0800

    refactored employee_leave_times controller

--- app/controllers/backend/employee_leave_times_controller.rb
+++ app/controllers/backend/employee_leave_times_controller.rb
@@ -1,24 +1,13 @@
 # frozen_string_literal: true
 class Backend::EmployeeLeaveTimesController < Backend::BaseController
-  before_action :custom_authorize
   skip_load_and_authorize_resource
 
   def index
-    @current_collection = EmployeeMonthlyStat.total_leave_times_hours(params[:year], params[:month])
-  end
-
-  private
-
-  def custom_authorize
+    @current_collection = collection_scope.total_leave_times_hours(params[:year], params[:month])
     authorize! action_name, @current_collection
-    authorize! action_name, @current_object
   end
 
   def collection_scope
-    if params[:id]
-      User
-    else
-      User.order(id: :desc)
-    end
+    EmployeeMonthlyStat
   end
 end
