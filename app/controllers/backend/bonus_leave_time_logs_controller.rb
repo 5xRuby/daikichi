0b627366486b08a38ef077855ffa0295e6ce1f45
Eric Lee <imdfmy@gmail.com>
2016-11-01 12:49:43 +0800

    deleted backend/bonus_leave_time_logs controller

--- app/controllers/backend/bonus_leave_time_logs_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-class Backend::BonusLeaveTimeLogsController < ApplicationController
-  def index
-  end
-
-  def update
-  end
-end
--- spec/controllers/backend/bonus_leave_time_logs_controller_spec.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-require "rails_helper"
-RSpec.describe Backend::BonusLeaveTimeLogsController, type: :controller do
-end
