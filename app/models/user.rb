0343e797b3b18a27ae67bbb7d7c39d707422be0f
Maxwell <destinydragon85614@gmail.com>
2017-03-27 02:01:02 -0500

    Alter user :role to enum type (#71)
    * Confirm enum test
    
    * Convert roles to SettingsLogic
    
    * Update sample YML file
    
    * Abbreviate FactoryGirl preface

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  ROLES = %i(manager hr employee contractor intern resigned pending).freeze
+  enum role: Settings.roles
+  # %i(manager hr employee contractor intern resigned pending)
 
   scope :filter_by_join_date, ->(month, date) {
     where(
@@ -53,9 +54,9 @@ class User < ApplicationRecord
       .approved)
   }
 
-  ROLES.each do |role|
+  Settings.roles.each do |key, role|
     define_method "is_#{role}?" do
-      self.role.to_sym == role
+      self.role.to_s == role
     end
   end
 
