2a9602d3b19af11e4026f167aa34857e25f4191e
Yusheng Li <YushengLi@users.noreply.github.com>
2017-04-07 10:51:23 +0800

    Manage translations for enum User#role with i18n (#87)

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.human_enum_value(enum_name, enum_value)
+    I18n.t("activerecord.attributes.#{model_name.i18n_key}.#{enum_name.to_s.pluralize}.#{enum_value}")
+  end
+
+  def self.enum_attributes_for_select(enum_name)
+    self.send(enum_name).map do |enum_value, _|
+      [I18n.t("activerecord.attributes.#{model_name.i18n_key}.#{enum_name}.#{enum_value}"), enum_value]
+    end
+  end
 end
