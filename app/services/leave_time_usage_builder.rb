407ee6e34955857f84831f131b227415c48b8e47
Yusheng Li <YushengLi@users.noreply.github.com>
2017-05-04 17:39:25 +0800

    Revert "Refactor: Using ActiveRecord transaction"
    This reverts commit cfcb5f2dd480544cd7177e2ff96f5bc5d661028b.

--- app/services/leave_time_usage_builder.rb
+++ app/services/leave_time_usage_builder.rb
@@ -14,26 +14,38 @@ class LeaveTimeUsageBuilder
   end
 
   def build_leave_time_usages
-    ActiveRecord::Base.transaction do
-      raise ActiveRecord::Rollback unless leave_time_covered_application_time_interval?
+    # 如果確定起點或結束時間沒有切到 leave_time 則回傳 false
+    return false unless leave_time_include_application_time_interval
 
-      @available_leave_times.each do |lt|
-        @used_hour_count = 0
-        iterate_leave_time_dates(lt) do |date|
-          next if corresponding_leave_hours_date_is_zero?(date)
-          if usable_hours_affordable?(lt, date)
-            fill_all_leave_hours_by_date(date)
-          else
-            fill_leave_hours_by_date_with_remain_leave_time(lt, date)
-            break
-          end
+    # 迭代 leave_times 並且開始扣除 leave_hours_by_date 之額度
+    @available_leave_times.each do |lt|
+      lt_used_hour_count = 0
+      
+      iterate_leave_time_dates(lt) do |date|
+        # 如果假單對應之日期之時間已經扣為 0，則跳至下一天
+        next if corresponding_leave_hours_date_is_zero?(date)
+        if lt_used_hour_count + @leave_hours_by_date[date] <= lt.usable_hours
+          # leave_time 如果夠使用則填補所有當日時數
+          lt_used_hour_count += @leave_hours_by_date[date]
+          @leave_hours_by_date[date] = 0
+        else
+          # leave_time usable_hours 不夠使用，直接跳出並紀錄新的 leave_time_usage
+          remain_hours = lt_used_hour_count + @leave_hours_by_date[date] - lt.usable_hours
+          lt_used_hour_count = lt.usable_hours
+          @leave_hours_by_date[date] = remain_hours
+          break
         end
-        @leave_time_usages.push(leave_time: lt, used_hours: @used_hour_count)
       end
 
-      raise ActiveRecord::Rollback if remain_leave_hours_by_date?
-      create_leave_time_usage_and_lock_hours
+      @leave_time_usages.push({ leave_time: lt, used_hours: lt_used_hour_count })
     end
+
+    # 檢查 leave_hours_by_date 是否還有殘餘的時數
+    return false if remain_leave_hours_by_date?
+    
+    # 產生 LeaveTimeUsage 以及相對應的 LeaveTime locked_hours
+    create_leave_time_usage_and_lock_hours
+    true
   end
 
   private
@@ -47,36 +59,22 @@ class LeaveTimeUsageBuilder
       .until(@leave_application.end_time).to_a
   end
 
-  def leave_time_include_application_time_interval?
+  # 檢查 leave_times 至少有切到 leave_application 之起點以及終點
+  def leave_time_include_application_time_interval
     include_start_time = include_end_time = false
     @available_leave_times.each do |lt|
       include_start_time = true if lt.cover?(@leave_application.start_time.to_date)
-      include_end_time = true if lt.cover?(@leave_application.end_time.to_date)
+      include_end_time   = true if lt.cover?(@leave_application.end_time.to_date)
     end
     include_start_time && include_end_time
   end
 
   def iterate_leave_time_dates(leave_time)
     la_start_date = @leave_application.start_time.to_date
-    la_end_date = @leave_application.end_time.to_date
-    start_date = la_start_date > leave_time.effective_date ? la_start_date : leave_time.effective_date
-    end_date = la_end_date < leave_time.expiration_date ? la_end_date : leave_time.expiration_date
+    la_end_date   = @leave_application.end_time.to_date
+    start_date = la_start_date > leave_time.effective_date  ? la_start_date : leave_time.effective_date
+    end_date   = la_end_date   < leave_time.expiration_date ? la_end_date   : leave_time.expiration_date
     start_date.upto(end_date) { |date| yield date }
-  end
-
-  def usable_hours_affordable?(leave_time, date)
-    @used_hour_count + @leave_hours_by_date[date] <= leave_time.usable_hours
-  end
-
-  def fill_all_leave_hours_by_date(date)
-    @used_hour_count += @leave_hours_by_date[date]
-    @leave_hours_by_date[date] = 0
-  end
-
-  def fill_leave_hours_by_date_with_remain_leave_time(leave_time, date)
-    remain_hours = @used_hour_count + @leave_hours_by_date[date] - lt.usable_hours
-    @used_hour_count = lt.usable_hours
-    @leave_hours_by_date[date] = remain_hours
   end
 
   def corresponding_leave_hours_date_is_zero?(date)
@@ -101,8 +99,9 @@ class LeaveTimeUsageBuilder
     leave_time_usage.used_hours = used_hours
     leave_time_usage.save!
   end
-
+  
   def lock_leave_time_hours(leave_time, used_hours)
     leave_time.lock_hours used_hours
   end
+  
 end
