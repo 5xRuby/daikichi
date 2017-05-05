16cae373206d8f72e9078d4ecfa298a71f4d5702
Maxwell-Alexius <destinydragon85614@gmail.com>
2017-05-05 12:01:21 +0800

    Lock leave time hours when successfully creating leave applications

--- app/models/leave_time_usage.rb
+++ app/models/leave_time_usage.rb
@@ -1,4 +1,13 @@
 class LeaveTimeUsage < ApplicationRecord
   belongs_to :leave_application
   belongs_to :leave_time
+
+  before_save :lock_leave_time_hours
+
+  private
+
+  def lock_leave_time_hours
+    leave_time = LeaveTime.find(self.leave_time_id)
+    leave_time.lock_hours!(self.used_hours)
+  end
 end
