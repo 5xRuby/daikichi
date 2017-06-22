69374c1e569e8ad1a480161c0281bbcb6b414fb1
Maxwell-Alexius <destinydragon85614@gmail.com>
2017-06-22 17:41:13 +0800

    #166 Resolved observer test issue

--- app/models/leave_application_observer.rb
+++ app/models/leave_application_observer.rb
@@ -2,6 +2,10 @@
 class LeaveApplicationObserver < ActiveRecord::Observer
   observe LeaveApplication
 
+  def before_validation(record)
+    assign_hours(record)
+  end
+
   def before_save(record)
     cleanup_leave_hours_by_date(record)
   end
@@ -10,7 +14,23 @@ class LeaveApplicationObserver < ActiveRecord::Observer
     create_leave_hours_by_date(record)
   end
 
+  def after_create(record)
+    create_leave_time_usages(record)
+  end
+
+  def before_update(record)
+    hours_transfer(record)
+  end
+
+  def after_update(record)
+    create_leave_time_usages(record) if record.aasm_event? :revise
+  end
+
   private
+
+  def assign_hours(record)
+    record.hours = record.send(:auto_calculated_minutes) / 60
+  end
 
   def cleanup_leave_hours_by_date(record)
     record.leave_hours_by_dates.delete_all if record.persisted? && record.interval_changed?
@@ -22,5 +42,37 @@ class LeaveApplicationObserver < ActiveRecord::Observer
       record.leave_hours_by_dates.build(date: date.first, hours: date.second)
     end
     LeaveHoursByDate.import leave_hours_by_dates, validate: true
+  end
+
+  def create_leave_time_usages(record)
+    raise ActiveRecord::Rollback unless LeaveTimeUsageBuilder.new(record).build_leave_time_usages
+  end
+
+  def hours_transfer(record)
+    case
+    when record.aasm_event?(:approve) then transfer_locked_hours_to_used_hours(record)
+    when record.aasm_event?(:reject)  then return_leave_time_usable_hours(record)
+    when record.aasm_event?(:cancel)  then return_leave_time_usable_hours(record)
+    when record.aasm_event?(:revise)  then return_leave_time_usable_hours(record)
+    end
+  end
+
+  def transfer_locked_hours_to_used_hours(record)
+    record.leave_time_usages.each { |usage| usage.leave_time.use_hours!(usage.used_hours) }
+  end
+
+  def return_leave_time_usable_hours(record)
+    record.leave_time_usages.each { |usage| revert_hours(record, usage) }
+    record.leave_time_usages.delete_all
+  end
+
+  def revert_hours(record, usage)
+    if record.aasm.from_state == :pending
+      usage.leave_time.unlock_hours!(usage.used_hours)
+    elsif record.aasm.from_state == :approved
+      usage.leave_time.unuse_hours!(usage.used_hours)
+    else
+      raise ActiveRecord::Rollback
+    end
   end
 end
