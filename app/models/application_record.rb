9b0e5cdf9d657c9974a7064b87d68fb20713c075
Maxwell <destinydragon85614@gmail.com>
2017-07-17 21:15:44 -0500

    issue #183 Hide fullpaid_sick and halfpaid_sick type in front-end (#184)

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -6,7 +6,7 @@ class ApplicationRecord < ActiveRecord::Base
     I18n.t("activerecord.attributes.#{model_name.i18n_key}.#{enum_name.to_s.pluralize}.#{enum_value}")
   end
 
-  def self.enum_attributes_for_select(enum_name, except: [])
+  def self.enum_attributes_for_select(enum_name, except = [])
     self.send(enum_name).reject { |x| except.include?(x.to_sym) }.map do |enum_value, _|
       [I18n.t("activerecord.attributes.#{model_name.i18n_key}.#{enum_name}.#{enum_value}"), enum_value]
     end
