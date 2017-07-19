6d8bbd94d1b9b626bae791eadcb134eb550c7c99
Awaru Ryudo <ryudoawaru@gmail.com>
2017-07-19 18:10:42 +0800

    Change production deploy settings

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-set :deploy_to, '/home/5xruby/daikichi.5xruby.tw'
-role :app, %w{5xruby@hq.5xruby.tw:14159}
-role :web, %w{5xruby@hq.5xruby.tw:14159}
-role :db, %w{5xruby@hq.5xruby.tw:14159}
+set :deploy_to, '/home/deploy/daikichi.5xruby.tw'
+role :app, %w{deploy@10.128.128.154}
+role :web, %w{deploy@10.128.128.154}
+role :db, %w{deploy@10.128.128.154}
