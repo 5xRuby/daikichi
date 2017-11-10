e250fcdc215c4a74e37a2c4335d8bc9e1c5534b3
Eileen0917 <j405307@gmail.com>
2017-11-10 16:23:42 +0800

    let user information separate from admin information

--- app/mailers/information_mailer.rb
+++ app/mailers/information_mailer.rb
@@ -1,11 +1,8 @@
 class InformationMailer < ApplicationMailer
+  @@hi_5xruby = "hi@5xruby.tw"
+
   def new_application(leave_application)
     @leave_application = leave_application
-    mail(from: "hi@5xruby.tw", to: "hi@5xruby.tw", subject: 'New Leave Application')
-  end
-
-  def send_approve(leave_application)
-    @leave_application = leave_application
-    mail(from: "hi@5xruby.tw", to: @leave_application.user.email, subject: 'Your Leave Application is Approved.')
+    mail(from: @@hi_5xruby, to: @@hi_5xruby, subject: 'New Leave Application')
   end
 end
