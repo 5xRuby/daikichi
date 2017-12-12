b77841d138f5c5d9238e2267199ad54bd5ddb388
telsaiori <telsaiori@gmail.com>
2017-12-12 10:31:48 +0800

    issue #235 add test mail to subject when sending mail on staging/development env (#245)

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,16 @@
 class UserMailer < ApplicationMailer
   def reply_leave_applicaiton_email(leave_application)
     @leave_application = leave_application
-
-    mail(
-      to: @leave_application.user.email,
-      subject: "<請假結果通知> 你的#{LeaveApplication.human_enum_value(:leave_type, @leave_application.leave_type)}已被#{LeaveApplication.human_enum_value(:status, @leave_application.status)}"
-    )
+    if %w(staging development).include?(Rails.env)
+      mail(
+        to: @leave_application.user.email,
+        subject: "[測試信]<請假結果通知> 你的#{LeaveApplication.human_enum_value(:leave_type, @leave_application.leave_type)}已被#{LeaveApplication.human_enum_value(:status, @leave_application.status)}"
+      )
+    else
+      mail(
+        to: @leave_application.user.email,
+        subject: "<請假結果通知> 你的#{LeaveApplication.human_enum_value(:leave_type, @leave_application.leave_type)}已被#{LeaveApplication.human_enum_value(:status, @leave_application.status)}"
+      )
+    end
   end
 end
