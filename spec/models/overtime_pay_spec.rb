f13495a5c5a9f4a79e304d002e76cdbdcee59200
DC <dachang.h@gmail.com>
2019-03-02 18:01:12 +0800

    Add model specs

---
+++ spec/models/overtime_pay_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe OvertimePay, type: :model do
+  describe '#associations' do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:overtime) }
+  end
+end
