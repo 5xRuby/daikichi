079b5802a74d0e005bfa1a34921cd2eda98f6add
DC <dachang.h@gmail.com>
2019-03-20 12:12:44 +0800

    Update OvertimesHelper #render_append_hours_options

--- app/helpers/backend/overtimes_helper.rb
+++ app/helpers/backend/overtimes_helper.rb
@@ -2,9 +2,9 @@
 module Backend
   module OvertimesHelper
     def render_append_hours_options
-      if current_object.may_approve? and current_object.leave?
+      if current_object.may_approve? && current_object.leave?
         link_to t("title.backend/overtimes.append_quota"), add_leave_time_backend_overtime_path, class: 'btn btn-warning'
-      elsif current_object.may_approve? and current_object.pay? && current_object.overtime_pay.nil?
+      elsif current_object.may_approve? && current_object.pay?
         link_to t("title.backend/overtimes.append_overtime_pay"), add_compensatory_pay_backend_overtime_path, class: 'btn btn-warning'
       end
     end
