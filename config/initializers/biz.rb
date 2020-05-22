03dc96540c05e21fdaf177f1aee0aee949239275
shes50103 <shes50103@gmail.com>
2020-05-22 15:29:21 +0800

    feat: update config/initializers/biz.rb

--- config/initializers/biz.rb
+++ config/initializers/biz.rb
@@ -1,7 +1,6 @@
 module Daikichi
   class Config
-    HOLIDAY_API = "https://data.ntpc.gov.tw/api/v1/rest/datastore/382000000A-000077-002"
-    
+
     Biz = ::Biz.configure do |config|
       config.hours = {
         mon: {'09:30' => '12:30', '13:30' => '18:30'},
@@ -11,9 +10,7 @@ module Daikichi
         fri: {'09:30' => '12:30', '13:30' => '18:30'}
       }
 
-      response = HTTParty.get(HOLIDAY_API)
-
-      config.holidays = response.parsed_response["result"]["records"].map { |record| Date.parse(record["date"]) }
+      config.holidays = Holiday.pluck(:date).map(&:to_date) if ActiveRecord::Base.connected?
 
       config.time_zone = 'Asia/Taipei'
     end
