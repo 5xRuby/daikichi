99c885452c1e36949fd59d0d77a2eee32f442af8
shes50103 <shes50103@gmail.com>
2020-05-22 15:03:13 +0800

    tmp

--- config/initializers/biz.rb
+++ config/initializers/biz.rb
@@ -1,22 +1,22 @@
 module Daikichi
   class Config
-    HOLIDAY_API = "https://data.ntpc.gov.tw/api/v1/rest/datastore/382000000A-000077-002"
+    # HOLIDAY_API = "https://data.ntpc.gov.tw/api/v1/rest/datastore/382000000A-000077-002"
 
-    Biz = ::Biz.configure do |config|
-      config.hours = {
-        mon: {'09:30' => '12:30', '13:30' => '18:30'},
-        tue: {'09:30' => '12:30', '13:30' => '18:30'},
-        wed: {'09:30' => '12:30', '13:30' => '18:30'},
-        thu: {'09:30' => '12:30', '13:30' => '18:30'},
-        fri: {'09:30' => '12:30', '13:30' => '18:30'}
-      }
+    # Biz = ::Biz.configure do |config|
+    #   config.hours = {
+    #     mon: {'09:30' => '12:30', '13:30' => '18:30'},
+    #     tue: {'09:30' => '12:30', '13:30' => '18:30'},
+    #     wed: {'09:30' => '12:30', '13:30' => '18:30'},
+    #     thu: {'09:30' => '12:30', '13:30' => '18:30'},
+    #     fri: {'09:30' => '12:30', '13:30' => '18:30'}
+    #   }
 
-      response = HTTParty.get(HOLIDAY_API)
+    #   response = HTTParty.get(HOLIDAY_API)
 
 
-      config.holidays = response.parsed_response["result"]["records"].map { |record| Date.parse(record["date"]) }
+    #   config.holidays = response.parsed_response["result"]["records"].map { |record| Date.parse(record["date"]) }
 
-      config.time_zone = 'Asia/Taipei'
-    end
+    #   config.time_zone = 'Asia/Taipei'
+    # end
   end
 end
