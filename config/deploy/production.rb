c3d9210446e927e43384b8f13ae891d0f3a65b77
蒼時弦也 <elct9620@frost.tw>
2020-08-18 17:12:40 +0800

    Update production server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :deploy_to, '/home/deploy/daikichi.5xruby.tw'
-role :app, %w{deploy@do.5xruby.tw}
-role :web, %w{deploy@do.5xruby.tw}
-role :db, %w{deploy@do.5xruby.tw}
+role :app, %w{deploy@do.5xruby.com}
+role :web, %w{deploy@do.5xruby.com}
+role :db, %w{deploy@do.5xruby.com}
