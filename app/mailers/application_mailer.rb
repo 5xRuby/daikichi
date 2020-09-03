0f73c33e612119be09a37bc86c0ad79238209599
蒼時弦也 <elct9620@frost.tw>
2020-09-03 16:38:18 +0800

    Allow configure mailer default sender via config

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'hi@5xruby.tw'
+  default from: Settings.mailer.default_from
 
   layout 'mailer'
   prepend_view_path Rails.root.join('app', 'views', 'mailers')
